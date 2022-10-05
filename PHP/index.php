0ee501da6d6271ffd02ceba0090b371e0131ea26
Simoniaque <simon.auriac@limayrac.fr>
2022-10-05 10:54:56 +0200

    Creation de la page d'accueil, de connexion,inscription et deconnexion en php avec leur css

---
+++ PHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FREDI</title>
+</head>
+<body>
+    <h1>FREDI - Accueil</h1>
+    <ul>
+        <li><a href="inscription.php"> Inscription</a></p></li>
+        <li><a href="connexion.php"> Connexion</a></p></li>
+        <li><a href="deconnexion.php"> Deconnexion</a></p></li>
+    </ul>
+</body>
+</html>
