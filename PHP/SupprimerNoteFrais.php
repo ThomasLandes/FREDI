2dd09e4f0135a1a69af648db3719715ddc911353
Paul Gauron <paul.gauron@limayrac.fr>
2023-05-27 19:25:35 +0200

    correction bug SupprimerNoteFrais

--- PHP/SupprimerNoteFrais.php
+++ PHP/SupprimerNoteFrais.php
@@ -18,9 +18,22 @@ $submit = isset($_POST['submit']);
 
 
 if ($submit) {
-    
+
     $idnote = isset($_POST['idnote'])? $_POST['idnote'] : null;
     $idligne = isset($_POST['idligne'])? $_POST['idligne'] : null;
+
+
+    try {
+        // Récupération des données de la ligne de frais
+        $sql2 = "SELECT * FROM lignefrais WHERE id_note = :idnote";
+        $params = array(":idnote" => $idnote);
+        $sth = $dbh->prepare($sql2);
+        $sth->execute($params);
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        $nb = $sth->rowCount();
+    } catch (PDOException $e) {
+        die("<p>Erreur lors de la requête SQL 5 : " . $e->getMessage() . "</p>");
+    }
 
     $sql = "DELETE FROM lignefrais where idligne = :idligne ";
 
@@ -33,7 +46,22 @@ if ($submit) {
         $sth = $dbh->prepare($sql);
         $sth->execute($params);
     } catch (PDOException $e) {
-        die("<p>Erreur lors de la requête SQL : " . $e->getMessage() . "</p>");
+        die("<p>Erreur lors de la requête SQL 12 : " . $e->getMessage() . "</p>");
+    }
+    if($nb == 1){
+        $sql1 = "DELETE FROM notefrais where id_note = :idnote";
+
+        $params = array(
+            ":idnote" => $idnote,
+
+        );
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+            $sth = $dbh->prepare($sql1);
+            $sth->execute($params);
+        } catch (PDOException $e) {
+            die("<p>Erreur lors de la requête SQL 22 : " . $e->getMessage() . "</p>");
+        }
     }
     header("Location: ListeNotefrais.php?id_note=$idnote");
      
@@ -46,7 +74,6 @@ if ($submit) {
         $sth = $dbh->prepare($sql);
         $sth->execute($params);
         $row = $sth->fetch(PDO::FETCH_ASSOC);
-
         // Remplissage des champs du formulaire avec les données de la ligne de frais
         $date = $row['datedeplacement'];
         $lib = $row['libDeplacement'];
@@ -56,9 +83,9 @@ if ($submit) {
         $fraisH = $row['fraisHeberge'];
         $fraisK= $row['fraisKilometre'];
     } catch (PDOException $e) {
-        die("<p>Erreur lors de la requête SQL : " . $e->getMessage() . "</p>");
+        die("<p>Erreur lors de la requête SQL 5 : " . $e->getMessage() . "</p>");
     }
-    
+
 }
 
 ?>
@@ -78,13 +105,13 @@ if ($submit) {
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer cette ligne de frais?');">
 <p><input type="hidden" name="idnote" value="<?php echo $idnote; ?>"></p>
 <p><input type="hidden" name="idligne" value="<?php echo $idligne; ?>"></p>
-  <p>Date Deplacement<br /><input type="date" name="date" value = "<?php echo $date?>"  ></p>
-  <p>Lib Deplacement<br /><input type="text" name="libdeplacement" value = "<?php echo $lib ?>"></p>
-  <p>Kilometrage<br /><input type="text" name="kilometrage" value = "<?php echo $kilo ?>" ></p>
-  <p>fraisPeage<br /><input type="text" name="fraisPeage" value = "<?php echo $fraisP  ?>" ></p>
-  <p>fraisRepas<br /><input type="text" name="fraisRepas"  value = "<?php echo $fraisR  ?>" ></p>
-  <p>fraisHeberge<br /><input type="text" name="fraisHeberge"  value = "<?php echo $fraisH ?>" ></p>
-  <p>fraisKilometre<br /><input type="text" name="fraisKilometre"  value = "<?php echo $fraisK  ?>"></p>
+  <p>Date Deplacement<br /><input type="date" name="date" value = "<?php echo $date?>"  disabled></p>
+  <p>Lib Deplacement<br /><input type="text" name="libdeplacement" value = "<?php echo $lib ?>" disabled></p>
+  <p>Kilometrage<br /><input type="text" name="kilometrage" value = "<?php echo $kilo ?>" disabled ></p>
+  <p>fraisPeage<br /><input type="text" name="fraisPeage" value = "<?php echo $fraisP  ?>" disabled></p>
+  <p>fraisRepas<br /><input type="text" name="fraisRepas"  value = "<?php echo $fraisR  ?>" disabled></p>
+  <p>fraisHeberge<br /><input type="text" name="fraisHeberge"  value = "<?php echo $fraisH ?>" disabled ></p>
+  <p>fraisKilometre<br /><input type="text" name="fraisKilometre"  value = "<?php echo $fraisK  ?>" disabled></p>
   <p><input type="submit" name="submit" value="OK"></p>
 </form>
 <?php
